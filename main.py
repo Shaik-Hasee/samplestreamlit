0e8c494da10eb978458700c967d55432dabb1253
Shaik-Hasee <98262166+Shaik-Hasee@users.noreply.github.com>
2023-12-29 10:09:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from PIL import Image
 st.write("<h1>This is my streamlit page</h1>",unsafe_allow_html=True)
 st.write("<h1 style='color:red;'>This is my streamlit page</h1>",unsafe_allow_html=True)
-st.file_uploader("upload a file")
+uploader_file=st.file_uploader("upload a file")
 st.image("https://rukminim1.flixcart.com/flap/96/96/image/22fddf3c7da4c4f4.png?q=100")
+Image=Image.open(uploader_file)
+st.image(image, caption='Image uploaded',use_column_width=True)
