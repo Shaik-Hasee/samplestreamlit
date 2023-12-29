3edafe45eccf6d40b7d9a11daf798e46c48b1870
Shaik-Hasee <98262166+Shaik-Hasee@users.noreply.github.com>
2023-12-29 10:00:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 streamlit.write("<h1>This is my streamlit page</h1>",unsafe_allow_html=True)
-streamlit.write("<h1 style='color':'red'>This is my streamlit page</h1>",unsafe_allow_html=True)
+streamlit.write("<h1 style='color:red;'>This is my streamlit page</h1>",unsafe_allow_html=True)
 st.file_uploader("upload a file")
 st.image("https://rukminim1.flixcart.com/flap/96/96/image/22fddf3c7da4c4f4.png?q=100")
