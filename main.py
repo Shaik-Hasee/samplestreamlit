7ea788a1062032cf49e0130f7397e21322cab0c5
Shaik-Hasee <98262166+Shaik-Hasee@users.noreply.github.com>
2023-12-29 09:56:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-import streamlit
-streamlit.write("This is my streamlit page")
+import streamlit as st
+streamlit.write("<h1>This is my streamlit page</h1>",unsafe_allow_html=True)
+streamlit.write("<h1 style='color':'red'>This is my streamlit page</h1>",unsafe_allow_html=True)
+st.file_uploader("upload a file")
+st.image('jpg','png',jpeg')
